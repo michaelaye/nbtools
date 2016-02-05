915ee91e6a7772400cf4568255e6d24f3300c2de
K.-Michael Aye <kmichael.aye@gmail.com>
2016-02-05 12:50:07 -0700

    fix typo

--- nbtools/__init__.py
+++ nbtools/__init__.py
@@ -1,5 +1,5 @@
 """Initialisation of nbtools package."""
 
 # convenience imports
-from .progessbars import multiprocess_progress_display
+from .progressbars import multiprocess_progress_display
 from .progressbars import ListProgressBar
