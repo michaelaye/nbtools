0f857f3564a49309f71599388a4d1d34943ac161
K.-Michael Aye <kmichael.aye@gmail.com>
2018-08-11 19:00:19 -0600

    util to setup live logging in a notebook

--- nbtools/logging.py
+++ nbtools/logging.py
@@ -2,10 +2,17 @@ import logging
 import sys
 
 
-def setup_live_logging(module, level):
+def setup_live_logging(module, level, fname=None):
     logger = logging.getLogger(module)
     ch = logging.StreamHandler(sys.stderr)
-    fmt = logging.Formatter('%(name)s - %(message)s')
+    fmt = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
     ch.setFormatter(fmt)
     logger.addHandler(ch)
+    if fname is not None:
+        fh = logging.FileHandler(fname)
+        fh.setLevel(level)
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)
     logger.setLevel(level)
+
+    return logger
